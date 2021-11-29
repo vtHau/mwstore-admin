024f263a4ceac7262a74c7b5e4869d58a3324be5
vtHau <crtrunghau@gmail.com>
2021-11-29 11:22:30 +0700

    Fix home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,21 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import useTitle from "../../hooks/useTitle";
 import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
 
 function Home() {
+  const admin = useSelector((state) => state.adminReducer.admin);
+
   useTitle("Home");
 
   return (
     <>
       <Breadcrumb title="Home" parent="Home" />
-      <div className="container-fluid">
-        <div className="card">
-          <div className="card-header">
-            <h5>Home</h5>
-          </div>
+      <div className="container-fluid" style={{ height: "500px" }}>
+        <div className="card" style={{ height: "100%" }}>
           <div className="card-body">
-            <div className="clearfix"></div>
-            <div className="product-physical"></div>
+            <h3 className="title-hello text-center">
+              Chào <b>{admin.name}</b> đã quay trở lại trang quản trị <br />
+              <b>MW Store</b>
+            </h3>
           </div>
         </div>
       </div>
